2ab5032cc1b9c9d4428e72c0c2c0d45cfe670521
xuzhiqiang <xuzhiqiang928@tops001.com>
2017-03-19 17:26:36 +0800

    update

---
+++ src/mockData/developer_list_mock_data.js
@@ -0,0 +1,73 @@
+var developer_list = {
+	Code: 0,
+	Data: {
+		count: 22,
+		items: [
+			{
+				developerId: '001',
+				developerName: '太原绿城华瑞地产开发商001',
+				developerContacts: '刘长江001',
+				developerPosition: '营销总监',
+				developerContactsPhone: '138-9898-9878',
+				childItems: [
+					{
+						subBuildingId: '001001',
+						subBuildingName: '双湖星城001',
+						subBuildingAdress: '迎泽区滨盛路与长河路交叉路口',
+						subBuildingContacts: '张小明',
+						subBuildingContactsPhone: '137 9898 9898',
+						subBuildingSurplusMonth: '12',
+						subBuildingSurplusDay: '22',
+						subBuildingSignDate: '2016-05-16',
+						subBuildingTotalDate: '25',
+					},
+					{
+						subBuildingId: '001002',
+						subBuildingName: '测试雅居乐长度',
+						subBuildingAdress: '迎泽区滨盛路与长河路交叉路口',
+						subBuildingContacts: '张小明',
+						subBuildingContactsPhone: '137 9898 9898',
+						subBuildingSurplusMonth: '12',
+						subBuildingSurplusDay: '22',
+						subBuildingSignDate: '2016-05-16',
+						subBuildingTotalDate: '25',
+					},
+					{
+						subBuildingId: '001003',
+						subBuildingName: '测试双湖星城测试地址',
+						subBuildingAdress: '迎泽区滨盛路与长河路交叉路口',
+						subBuildingContacts: '王晓东',
+						subBuildingContactsPhone: '137 9898 9898',
+						subBuildingSurplusMonth: '12',
+						subBuildingSurplusDay: '22',
+						subBuildingSignDate: '2016-05-16',
+						subBuildingTotalDate: '18',
+					}
+				]
+			},
+			{
+				developerId: '001',
+				developerName: '太原绿城华瑞地产开发商002',
+				developerContacts: '刘长江测试002',
+				developerPosition: '营销总监',
+				developerContactsPhone: '138-9898-9878',
+				childItems: [
+					{
+						subBuildingId: '001002',
+						subBuildingName: '雅居乐长度002',
+						subBuildingAdress: '迎泽区滨盛路与长河路交叉路口',
+						subBuildingContacts: '张小明',
+						subBuildingContactsPhone: '137 9898 9898',
+						subBuildingSurplusMonth: '12',
+						subBuildingSurplusDay: '22',
+						subBuildingSignDate: '2016-05-16',
+						subBuildingTotalDate: '25',
+					}
+				]
+			}
+		]
+	},
+	Message: '',
+}
+
+module.exports = developer_list;
