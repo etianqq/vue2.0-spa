118d036c71676bff07a87718e450d7d4cb468e00
zhouchen3000 <zhouchen3000@tops001.com>
2017-03-27 20:00:46 +0800

    update

--- src/router/broker-company/broker-company.route.js
+++ src/router/broker-company/broker-company.route.js
@@ -4,12 +4,19 @@
 const brokerCompanyComponent = resolve => {
   require(['@/components/broker-company/broker-company.vue'], resolve)
 };
-
+const brokerAddCompanyComponent = resolve => {
+  require(['@/components/broker-company/broker-addCompany.vue'], resolve)
+};
 const brokerCompanyRoutes = [
   {
     path: 'broker-company',
     name: 'broker-company',
     component: brokerCompanyComponent
+  },
+  {
+    path: 'broker-addCompany',
+    name: 'broker-addCompany',
+    component: brokerAddCompanyComponent
   }
 ];
 
